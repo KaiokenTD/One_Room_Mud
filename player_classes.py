2559b0abaa6da1293cd5e574170daceac5f178c1
KaiokenTD <125826952+KaiokenTD@users.noreply.github.com>
2023-02-28 19:18:07 -0800

    Implemented Game Areas Data Struc
    Implemented nested dictionary containing relevant game areas and room information. Will be used to validate players current location to interact with objects.

--- player_classes.py
+++ player_classes.py
@@ -7,7 +7,14 @@ class Player:
         self._player_name = player_name
         self._hp = hp
         self._max_hp = max_hp
-        self._player_id = player_id
-        self._room_id = room_id
         self._dead = dead
 
+
+class ConnectingPlayer(Player):
+    """
+    Root object for player connecting to the game
+    """
+    def __init__(self, player_id, room_id, player_name, hp, max_hp):
+        super().__init__(player_name, hp, max_hp, player_id, room_id)
+        self._player_id = player_id
+        self._room_id = room_id
--- room_classes.py
+++ room_classes.py
@@ -1,16 +1,9 @@
-the_arena = {
-    "The Arena": {
-        "The arena floor is soaked in blood.": ["None"]
-    }
-}
-
-
 class Room:
     """
     Root room object
     """
-    def __init__(self, room_name, description, exits, room_id):
+    def __init__(self, room_name, description, room_id, exits):
         self._room_name = room_name
         self._description = description
-        self._exits = exits
         self._room_id = room_id
+        self._exits = exits
